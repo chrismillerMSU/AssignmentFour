a1b1668007dcd7560e3d11b825bd88cc69cc8aff
namesconnor <connormorrison@montana.edu>
2018-04-23 21:41:34 -0600

    Updated CoinTest.java, added more comments

--- program4/CoinTest.java
+++ program4/CoinTest.java
@@ -12,12 +12,14 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class CoinTest {
-
+	
+	//running tests to return arrays of change.
 	@Test
 	void testMinCoinsUS24() {
 		int change = 24;
 		int changeArray[] = {10,10,1,1,1,1};
 		int coins[] = {1,5,10,25};
+		//return an array
 		assertArrayEquals(changeArray, ChangeFinder.getChange(change,coins));
 	}
 	
@@ -26,6 +28,7 @@ class CoinTest {
 		int change = 99;
 		int changeArray[] = {25,25,25,10,10,1,1,1,1};
 		int coins[] = {1,5,10,25};
+		//return an array
 		assertArrayEquals(changeArray, ChangeFinder.getChange(change,coins));
 	}
 	
@@ -34,6 +37,7 @@ class CoinTest {
 		int change = 52;
 		int changeArray[] = {25,25,1,1};
 		int coins[] = {1,5,10,25};
+		//return an array
 		assertArrayEquals(changeArray, ChangeFinder.getChange(change,coins));
 	}
 	
@@ -42,13 +46,16 @@ class CoinTest {
 		int change = 16;
 		int changeArray[] = {5,5,5,1};
 		int coins[] = {1,5,12,25};
+		//return an array
 		assertArrayEquals(changeArray, ChangeFinder.getChange(change,coins));
 	}
 	
+	//test if there is no coins, and throw exception.
 	@Test
 	void testNoCoins() {
 		int change = 12;
 		int coins[] = {};
+		//throw an exception
 		Exception e = assertThrows(IllegalArgumentException.class, () -> {ChangeFinder.getChange(change,coins); });
 		assertEquals("No change is needed from 0 cents", e.getMessage());
 	}
